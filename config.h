4891060cb1c6ae7fc2e48f2212cedef64b681f3e
Robert Bisewski <contact@ibiscybernetics.com>
2017-03-29 09:17:34 -0500

    updating user agent

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static bool debug_mode  = false;
 // User agent of browser.
 static char *useragent  = "Mozilla/5.0 (X11; U; Unix; en-US) "
                           "AppleWebKit/602.4.8 (KHTML, like Gecko) "
-                          "Chrome/57.0.2987.98 Safari/602.4.8 Sighte/"VERSION;
+                          "Chrome/57.0.2987.110 Safari/602.4.8 Sighte/"VERSION;
 
 // Default page title
 static const char *default_page_title = "sighte Browser";
