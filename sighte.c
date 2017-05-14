0ca5b25e3a7cd82352dfcb86da8ac643877ffab8
Robert Bisewski <contact@ibiscybernetics.com>
2017-05-14 01:31:06 -0500

    in debug mode the program will not fork; this is done to allow for easier debugging

--- sighte.c
+++ sighte.c
@@ -3405,12 +3405,15 @@ void zoom(Client *c, const Arg *arg)
 int main(int argc, char *argv[])
 {
     // Create a fork, this will end up being the background process for the
-    // sighte that is ran in the foreground.
-    if (fork() != 0) {
-        print_debug("main() --> fork() has returned a non-zero value here. "
-                    "So the original process has *probably* passed through "
-                    "here intact.");
-        return 0;
+    // sighte that is ran in the foreground. Note that in debug mode this
+    // is skipped.
+    if (!debug_mode) {
+        if (fork() != 0) {
+            print_debug("main() --> fork() has returned a non-zero value here. "
+                        "So the original process has *probably* passed through "
+                        "here intact.");
+            return 0;
+        }
     }
 
     // Variable declaration
